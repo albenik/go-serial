8f0075f78f22c7d387b72e63f0eb98378e1b3982
rede97 <m454078356@163.com>
2019-03-04 10:52:01 +0800

    gen syscall for setupDiGetDeviceRegistryProperty

--- enumerator/syscall_windows.go
+++ enumerator/syscall_windows.go
@@ -6,4 +6,4 @@ package enumerator
 //sys setupDiEnumDeviceInfo(set hDevInfo, index uint32, info *pspDevInfoData) (err error) = setupapi.SetupDiEnumDeviceInfo
 //sys setupDiGetDeviceInstanceId(set hDevInfo, devInfo *pspDevInfoData, devInstanceId unsafe.Pointer, devInstanceIdSize uint32, requiredSize *uint32) (err error) = setupapi.SetupDiGetDeviceInstanceIdW
 //sys setupDiOpenDevRegKey(set hDevInfo, devInfo *pspDevInfoData, scope dicsScope, hwProfile uint32, keyType uint32, samDesired regsam) (hkey syscall.Handle, err error) = setupapi.SetupDiOpenDevRegKey
-//sys setupDiGetDeviceRegistryProperty(set hDevInfo, devInfo *pspDevInfoData, property deviceProperty, propertyType *uint32, outValue *byte, outSize *uint32, reqSize *uint32) (res bool) = setupapi.SetupDiGetDeviceRegistryPropertyW
+//sys setupDiGetDeviceRegistryProperty(set hDevInfo, devInfo *pspDevInfoData, property deviceProperty, propertyType *uint32, outValue *byte, outSize uint32, reqSize *uint32) (res bool) = setupapi.SetupDiGetDeviceRegistryPropertyW
