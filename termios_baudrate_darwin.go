785fc66f9c1533bc30c0395d544f3348acc9ba10
smihica <smihica@gmail.com>
2020-05-12 21:12:30 +0900

    Supported extended baudrate in OSX (darwin).

---
+++ termios_baudrate_darwin.go
@@ -0,0 +1,14 @@
+//
+// Copyright 2014-2017 Cristian Maglie. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+// +build darwin
+
+package serial
+
+func (s *settings) setBaudrate(speed int) error {
+	s.specificBaudrate = speed
+	return nil
+}
